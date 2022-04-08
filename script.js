cf5a770d3760132a76b974585d6afd88cbf0b0fd
YEONG LEE <1037480830@qq.com>
2022-04-08 14:27:48 +0800

    exercise-6

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.write("Hello,world!");
 
 function sayHello(){
 var response = prompt("What's your name?");
-alert("Hello" + response + ", have a nice day!");
+alert("Hello " + response + ", have a nice day!");
 }
 
 function add(a,b){
@@ -13,7 +13,6 @@ console.log(add(30,500));
 
 function showTopic(){
   var x = document.getElementById('demo');
-  <p id="demo" style="font-size: 25px; color:red;">Click the button to change the color and font.</p>
   x.style.fontSize ="25px";
   x.style.color ="red";
 }
